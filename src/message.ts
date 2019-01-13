98bade6c3be7f641b4bf6f0ca726cfd10c30dc4e
wandyezj <wandyezj@gmail.com>
2019-01-13 12:23:19 -0800

    add message command interface

---
+++ src/message.ts
@@ -0,0 +1,5 @@
+
+export interface Message {
+    command?: string;
+    data?: string;
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import * as express from "express";
 import * as WebSocket from "ws";
 
+import {Message} from "message";
+import { isUndefined } from "util";
+
 
 const app = express();
 
@@ -37,12 +40,51 @@ function connect(ws: WebSocket) {
     log("connection");
 
     ws.on("message", (data: string) => {
-        // ping back
-        // send the sent data back through the same connection
-        ws.send("Server Says: " + data);
+
+        handleMessage(ws, data);
     });
 }
 
+type Command =  (ws: WebSocket, data: string) => void;
+
+const registeredCommands: {[name: string]: Command} = {
+    "unknown": commandUnknown,
+    "ping": commandPing
+}
+
+
+function handleMessage(ws: WebSocket, data: string) {
+
+    let message: Message = {};
+
+    try {
+        message = JSON.parse(data) as Message;
+    } catch {
+    }
+
+    let commandName: string = message.command;
+    const commandData: string = message.data;
+
+    // Unknown Command
+    if (isUndefined(commandName) || !registeredCommands.hasOwnProperty(commandName)) {
+        commandName = "unknown";
+    }
+
+    const command: Command = registeredCommands[commandName];
+    command(ws, commandData);
+}
+
+function commandUnknown(ws: WebSocket, data: string) {
+    const message = "Unknown Command";
+    ws.send("Server Says: " + message);
+}
+
+function commandPing(ws: WebSocket, data: string) {
+    const message = data;
+    // ping back
+    // send the sent data back through the same connection
+    ws.send("Server Says: " + message);
+}
 
 function log(message: string) {
     console.log(message);
